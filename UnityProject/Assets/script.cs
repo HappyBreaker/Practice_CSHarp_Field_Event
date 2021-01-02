e575caa74de55179a08d58e7fd8bfd2dd957a8bf
HappyBreaker <75553500+HappyBreaker@users.noreply.github.com>
2021-01-03 04:02:57 +0800

    Down

---
+++ UnityProject/Assets/script.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class script : MonoBehaviour
+{
+    #region [宣告]
+    [Header("移動速度")]
+    [Range(0f,50f)]
+    public float Speed;
+    [Header("跳躍高度")]
+    [Range(0, 200)]
+    public int Junp;
+    [Header("講話內容")]
+    public string Talk;
+    [Header("是否取得雞蛋？")]
+    public bool Egg = false;
+    #endregion
+
+    void Start()
+    {
+        print("遊戲開始!!!");
+    }
+
+    
+    void Update()
+    {
+        Talk = "咕咕咕~~~";
+        print(Talk);
+    }
+}
